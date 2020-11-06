e138b1bfadae4f99f9c0472d40f34a8e4a695e8d
Dominik <domi.diner@gmail.com>
2020-11-06 12:09:19 +0100

    added python implementation

--- session-01/task-02/word_index.py
+++ session-01/task-02/word_index.py
@@ -17,13 +17,47 @@
 import sys, string
 
 # Global "constants"
-LINES_PER_PAGE       =  45
-MAX_SIZE_LINE        =  80
+LINES_PER_PAGE = 45
+MAX_SIZE_LINE = 80
 STOP_FREQUENCY_LIMIT = 100
+
+
+def lineIndexToPage(lineIndex):
+    return lineIndex / LINES_PER_PAGE + 1
+
 
 # Defining a main method makes testing easier
 def main(file_path):
-    pass
+    file = open(file_path)
+
+    wordsToOccurrences = {}
+    lineIndex = 0
+    for line in file:
+        line = line.replace('\n', '')
+        page = lineIndexToPage(lineIndex)
+        words = line.split(' ')
+
+        for word in words:
+            if word:
+                if word in wordsToOccurrences:
+                    occurrences = wordsToOccurrences[word]
+                else:
+                    occurrences = []
+
+                if page not in occurrences:
+                    occurrences.append(page)
+
+                wordsToOccurrences[word] = occurrences
+
+    words = sorted(wordsToOccurrences.keys())
+
+    for word in words:
+        occurrences = wordsToOccurrences[word]
+
+        if len(occurrences) <= STOP_FREQUENCY_LIMIT:
+            print(word, wordsToOccurrences[word])
+
+
 
 if __name__ == "__main__":
     main(sys.argv[1])
